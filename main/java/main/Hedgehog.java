caa85b522e88c93f2234894d71811cdfda390800
SamymMohibby <156694404+SamymMohibby@users.noreply.github.com>
2024-01-22 22:15:40 +0200

    Add files via upload

---
+++ main/java/main/Hedgehog.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Hedgehog {
+    String name;
+    int age;
+
+    public Hedgehog() {
+        name = "Pikseli";
+        age = 5;
+
+    }
+
+    public void speak() {
+        System.out.println("Siilin nimi:" + name);
+        
+    }
+}
