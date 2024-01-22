f46e259a8da0ffe76599bb2776012d1039f829ec
SamymMohibby <156694404+SamymMohibby@users.noreply.github.com>
2024-01-22 22:12:04 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+
+        Hedgehog hedgehog = new Hedgehog();
+        hedgehog.speak();
+    }
+
+    
+
+}
